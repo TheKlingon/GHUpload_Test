be2f3b63f59671154a9958000040b85001a09e01
Alain Natalini <the_klingon@live.it>
2018-10-05 08:46:47 +0200

    first commit

---
+++ GHUpload_Test/GHUpload_Test/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GHUpload_Test.Models;
+
+namespace GHUpload_Test.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
